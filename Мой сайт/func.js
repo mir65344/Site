521b05229bf3b7601fe6466544eb3b6cb1ea9914
Mirzhalol <mirzalolmirzobekov6@gmail.com>
2024-05-20 10:41:56 +0500

    Add files via upload

---
+++ Мой сайт/func.js
@@ -0,0 +1,11 @@
+/*<script>
+        function checkEmail() {
+            let email = document.querySelector('#emailField').value;
+            if(!email.includes('@')) alert('Нет символа @');
+            else if(!email.includes('.')) alert('Нет символа точки');
+            else if(!email.includes('com')) alert('Такой почты не существует');
+            else if(!email.includes('mail')) alert('Такой почты не существует');
+            else if(email.includes('yahoo')) alert('Используйте только Gmail и mail!')
+            else alert('Всё отлично!');
+        }
+    </script>*/
